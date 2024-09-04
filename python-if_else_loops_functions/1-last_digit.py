748ab15dbdf006f50df1beb2ba0ec6232cd3e0e9
Edison0341 <edison0341@gmail.com>
2024-09-04 13:07:21 -0400

    fixed pycostyle for task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,4 +10,5 @@ if number % 10 == 0:
 elif number % 10 > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif number % 10 < 6:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and\
+ is less than 6 and not 0")
