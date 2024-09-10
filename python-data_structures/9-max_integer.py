2b70a6b1be19ed20da7e38b2a479df56186c7517
Edison0341 <edison0341@gmail.com>
2024-09-10 13:00:05 -0400

    added a line of code

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if not my_list:
+        return (0, None)
     maximus = my_list[0]
-
     for item in my_list[1:]:
         if item > maximus:
             maximus = item
