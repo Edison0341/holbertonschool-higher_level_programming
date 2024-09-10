f3d4b4c304de050794bf6a72222b075f080c4792
Edison0341 <edison0341@gmail.com>
2024-09-10 11:23:27 -0400

    first push task 7

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+                print("", end="")
+        print()
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
