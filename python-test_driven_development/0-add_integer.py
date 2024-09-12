c33401152ab69016e0ac87621e077934072d6299
Edison0341 <edison0341@gmail.com>
2024-09-12 15:06:30 -0400

    first push task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that adds two integers"""
+
+def add_integer(a, b=98):
+    """
+    Function that adds
+    """
+    error = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if a is None or b is None:
+        raise TypeError(error)
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
