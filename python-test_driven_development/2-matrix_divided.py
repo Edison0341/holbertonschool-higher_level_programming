5fe75d90ca656d3f013c281caf1c392ce42f0d2d
Edison0341 <edison0341@gmail.com>
2024-09-12 19:05:18 -0400

    trying different code task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""comment"""
+"""Divide all elements of a matrix by a given number and round to 2 decimal
+    places."""
 
 
 def matrix_divided(matrix, div):
-    """comment"""
-    e = "matrix must be a matrix (list of lists) of integer/floats"
+    """Divide all elements of a matrix by a given number and round to 2 decimal
+    places."""
+    e = "matrix must be a matrix (array of arrays of integers/floats)"
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if not isinstance(matrix, list) or not all(isinstance(row,list)
+    for row in matrix):
+        raise TypeError(e)
+    row_length = len(matrix[0])
+    if not all(len(row) == row_length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     for row in matrix:
-        if type(row) is not list:
+        if not all(isinstance(item, (int, float)) for item in row):
             raise TypeError(e)
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for idx in row:
-            if type(idx) not in (int, float):
-                raise TypeError(e)
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
