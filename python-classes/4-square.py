4babbff87cbda49a385d84ca77fd8a775dca4e1b
Edison0341 <edison0341@gmail.com>
2024-09-16 14:04:01 -0400

    change placement of some function

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,6 +9,10 @@ class Square:
         """Initialize a square with size validation"""
         self.size = size
     
+    def area(self):
+        """Calculate and return the area of the square"""
+        return self.__size ** 2
+    
     @property
     def size(self):
         """Getter for the size attribute"""
@@ -22,7 +26,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """Calculate and return the area of the square"""
-        return self.__size ** 2
