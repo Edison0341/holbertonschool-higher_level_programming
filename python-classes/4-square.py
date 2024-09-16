0b610464fe473f5b8db413f725026b8a7d208a0f
Edison0341 <edison0341@gmail.com>
2024-09-16 13:51:56 -0400

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is a square class"""
+
+
+class Square:
+    """Square class with a private size attribute"""
+
+    def __init__(self, size=0):
+        """Initialize a square with size validation"""
+        self.size = size
+    
+    @property
+    def size(self):
+        """Getter for the size attribute"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square"""
+        return self.__size ** 2
