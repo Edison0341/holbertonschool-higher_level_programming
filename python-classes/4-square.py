4bc297ffbabce1d72f933d1bd52828a92fcfea88
Edison0341 <edison0341@gmail.com>
2024-09-16 14:07:18 -0400

    fixed pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,20 +7,24 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a square with size validation"""
+
         self.size = size
-    
+
     def area(self):
         """Calculate and return the area of the square"""
+
         return self.__size ** 2
-    
+
     @property
     def size(self):
         """Getter for the size attribute"""
+
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Setter for the size attribute with validation"""
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
