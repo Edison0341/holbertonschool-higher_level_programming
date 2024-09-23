cbdce72a80b345266e16da5d02407b79d25ab4f0
Edison0341 <edison0341@gmail.com>
2024-09-23 12:08:22 -0400

    task 1 and documentation file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class MyList that inherits from list"""
+
+class MyList(list):
+    """A list subclass with a method to print a sorted list"""
+    def print_sorted(self):
+        """Print the list in ascending order"""
+        print(sorted(self))
