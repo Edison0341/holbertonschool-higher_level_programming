896e3834cbd7e8ef1ee9e0fd28b73575053e66b6
Edison0341 <edison0341@gmail.com>
2024-09-23 12:09:41 -0400

    fixed pycodestyle

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class MyList that inherits from list"""
 
+
 class MyList(list):
     """A list subclass with a method to print a sorted list"""
     def print_sorted(self):
