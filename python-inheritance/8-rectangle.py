7eb01e62500ca54c73fe6347f98ee28688bfec26
Edison0341 <edison0341@gmail.com>
2024-09-24 11:30:43 -0400

    change code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,23 +11,18 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates that the value is a positive integer"""
-        self.name = name
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        self.value = value
 
 
 class Rectangle(BaseGeometry):
     """Represents a rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
+        """Initialize width and height with validation"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        """String representation of the rectangle"""
-        return (f"[Rectangle] {self.__width}/{self.__height}")
