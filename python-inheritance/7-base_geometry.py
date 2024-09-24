679adb3312f0ad45543ed67db6d6247e65620f0e
Edison0341 <edison0341@gmail.com>
2024-09-24 10:02:05 -0400

    task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""BaseGeometry class module"""
+
+
+class BaseGeometry:
+    """Represents the base class for geometry-related operations"""
+
+    def area(self):
+        """Error indicatin that the area method is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that the value is a positive integer"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
