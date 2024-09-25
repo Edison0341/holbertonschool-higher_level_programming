dbfdd7ba8ec4fa076f3870f6d9e718314bdb600e
Edison0341 <edison0341@gmail.com>
2024-09-25 11:25:00 -0400

    fixed some codes in the clases

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,31 +19,30 @@ class Shape(ABC):
 class Circle(Shape):
     """Concrete class representing a circle"""
 
-    def __init__(self, radius):
-        self.radius = radius
+    def __init__(self, radius=0):
+        if not isinstance(radius, (int, float)):
+            self.radius = abs(radius)
 
     def area(self):
-        """Return the area of the circle"""
         return (math.pi * self.radius ** 2)
 
     def perimeter(self):
-        """Return the perimeter of the cirlce"""
         return (2 * math.pi * self.radius)
 
 
 class Rectangle(Shape):
     """Concrete class representing a rectangle"""
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, (int, float)):
+            self.width = width
+        if not isinstance(height, (int, float)):
+            self.height = height
 
     def area(self):
-        """Return the area of the rectangle"""
         return (self.width * self.height)
 
     def perimeter(self):
-        """Return the perimeter of the rectangle"""
         return (2 * (self.width + self.height))
 
 
