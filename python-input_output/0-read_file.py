1ed1c19db558ddad7ec5fa36750b1bcb388883e6
Edison0341 <edison0341@gmail.com>
2024-09-30 12:48:44 -0400

    change some code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""txt"""
+"""Reads a UTF-8 txt file"""
+
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints it to stdout"""
 
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
-        print(content)
+        print(content, end="")
