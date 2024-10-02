c114ef62af17905c6ee55f46f5207156ec472b5b
Edison0341 <edison0341@gmail.com>
2024-10-02 11:21:24 -0400

    task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,21 @@
+#!/bin/usr/python3
+"""module that reads data from CSV file to JSON format"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """Converts a CSV file to a JSON"""
+    data = []
+    try:
+        with open(filename, "r", encoding="utf-8") as csvfile:
+            csvReader = csv.DictReader(csvfile)
+            for rows in csv.DictReader:
+                data.append(rows)
+
+        with open("data.json", "w", encoding="utf-8") as file:
+            json.dump(data, file)
+            return True
+
+    except Exception:
+        return False
