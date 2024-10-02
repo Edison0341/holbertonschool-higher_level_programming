9ee3216a6ec37d49ba37d30811d3e60ea14354e9
Edison0341 <edison0341@gmail.com>
2024-10-02 10:27:41 -0400

    added documentations to methods

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,11 +4,13 @@ import pickle
 
 
 def serialize_and_save_to_file(data, filename):
+    """Serializes the given data and saves it to a file"""
     with open(filename, "wb") as file:
         pickle.dump(data, file)
 
 
 def load_and_deserialize(filename):
+    """Loads and deserializes data from a pickle file"""
     with open(filename, "rb") as file:
         load_data = pickle.load(file)
     return (load_data)
