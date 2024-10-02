ab0dca21d9d3a20b6251810f88a2c8960c85860e
Edison0341 <edison0341@gmail.com>
2024-10-02 10:32:06 -0400

    change code

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import pickle
 """Module for serializing and deserializin data"""
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """Serializes the given data and saves it to a file"""
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """Loads and deserializes data from a pickle file"""
-    with open(filename, "rb") as file:
-        load_data = pickle.load(file)
+    with open(filename, "r", encoding="utf-8") as file:
+        load_data = json.load(file)
     return (load_data)
