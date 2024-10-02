8d80c4756d10fc2bc70e6cc1176caecb74a9afde
Edison0341 <edison0341@gmail.com>
2024-10-02 10:47:32 -0400

    task 01

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""txt"""
+import pickle
+
+
+class CustomObject:
+    """custom class"""
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age},
+              Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as file:
+            load_data = pickle.load(file)
+        return (load_data)
