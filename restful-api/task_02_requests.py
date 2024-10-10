79140b17b889e933cacca24c4970052d9f81da3a
Edison0341 <edison0341@gmail.com>
2024-10-10 12:26:38 -0400

    try 4

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,32 +3,34 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
 
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    if r.status_code == 200:
+    print(f"Status Code:{r.status_code}")
+    if r.ok:
         posts = r.json()
-        print("Titles for all posts:\n")
         for post in posts:
             print(f"{post['title']}")
     else:
-            print(f"Failed to fetch posts. Status code: {r.status_code}")
+        print(f"Failed to fetch posts. Status code: {r.status_code}")
+
 
 def fetch_and_save_posts():
-    
+
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     if r.status_code == 200:
         posts = r.json()
-        structured_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        structured_posts = [{'id': post['id'], 'title': post['title'],
+                            'body': post['body']} for post in posts]
 
-        with open('posts.csv', mode='w', newline='', encoding='utf-8') as csv_file:
-                fieldnames = ['id', 'title', 'body']
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                
-                writer.writeheader()
-                writer.writerows(structured_posts)
+        with open('posts.csv', mode='w', newline='',
+                  encoding='utf-8') as csv_file:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_posts)
         print("Data has benn succesfully written to posts.csv")
     else:
         print(f"Failed to fetch posts. Status code: {r.status_code}")
-        
-
