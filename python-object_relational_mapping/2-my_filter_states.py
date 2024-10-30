6fb2c4aec07a3e734ae7ced685ccac56a5e22323
Edison0341 <edison0341@gmail.com>
2024-10-30 13:15:10 -0400

    task 2try 1

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""List all states in a database"""
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database_name):
+    """connects to MySQL server"""
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM states\
+                        WHERE name = BINARY '{}' ORDER BY id ASC".format(search))
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL or executing query: {e}")
+
+
+if __name__ == "__main__":
+    list_states(sys.argv[1], sys.argv[2], sys.argv[3])
