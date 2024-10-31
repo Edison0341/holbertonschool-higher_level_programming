a0d021db87903c6a15868e103048edb350f3459b
Edison0341 <edison0341@gmail.com>
2024-10-31 14:38:23 -0400

    change code again

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
-from model_state import Base, State
+"""First state model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """Base class for the states table"""
+    __tablename__ = "states"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
